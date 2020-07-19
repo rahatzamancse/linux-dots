75f20f4b8c82ee44928d16dce4dc838372783a90
Rahat Zaman <rahatzamancse@gmail.com>
2020-07-19 09:33:18 +0600

    Added rofi search and shortcut to it

--- .dots-git/scripts/01-install-pkgs.sh
+++ .dots-git/scripts/01-install-pkgs.sh
@@ -1,7 +1,7 @@
 yay -Rs manjaro-zsh-config
 yay -Rns $(pacman -Qtdq)
 
-yay -S --needed arandr autorandr betterlockscreen bspwm xorg-xbacklight xorg-xinput blueman bluez-utils dex dunst dunstify feh figlet lolcat flameshot gnome-keyring greenclip hibernator htop tlp ttf-jetbrains-mono jq kitty lsd nautilus networkmanager network-manager-applet noto-fonts powertop pamac papirus-icon-theme pulseaudio pulseaudio-bluetooth pavucontrol picom-ibhagwan-git python-pywal mate-polkit polybar rofi rofi-calc rofi-dmenu rofimoji rofi-file-browser-extended rofi-greenclip rofi-scripts sxhkd sysstat ttf-breeze-sans udiskie vim wmname wpgtk xdg-user-dirs xdo xdotool xorg-xev xorg-xinit xorg-xsetroot xsettingsd yay zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting zsh-theme-powerlevel10k zsh-auto-notify zsh-you-should-use
+yay -S --needed arandr autorandr betterlockscreen bspwm xorg-xbacklight xorg-xinput blueman bluez-utils dex dunst dunstify feh figlet lolcat flameshot gnome-keyring greenclip hibernator htop tlp ttf-jetbrains-mono jq kitty lsd nautilus networkmanager network-manager-applet noto-fonts powertop pamac papirus-icon-theme pulseaudio pulseaudio-bluetooth pavucontrol picom-ibhagwan-git python-pywal mate-polkit polybar rofi rofi-search googler rofi-calc rofi-dmenu rofimoji rofi-file-browser-extended rofi-greenclip rofi-scripts sxhkd sysstat ttf-breeze-sans udiskie vim wmname wpgtk xdg-user-dirs xdo xdotool xorg-xev xorg-xinit xorg-xsetroot xsettingsd yay zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting zsh-theme-powerlevel10k zsh-auto-notify zsh-you-should-use
 
 xdg-user-dirs-update
 yay -Rns $(pacman -Qtdq)
