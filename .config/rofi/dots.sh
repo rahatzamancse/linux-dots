3bc6c6ec05040c3d97688b1371dd38660f78d5d6
Rahat Zaman <rahatzamancse@gmail.com>
2020-07-26 10:29:54 +0600

    Added tesseract

--- .config/rofi/dots.sh
+++ .config/rofi/dots.sh
@@ -5,5 +5,5 @@ DOTS_GIT=$HOME/.dots-git/git-control/.git
 
 dotfile_paths=$(/usr/bin/git --git-dir=$DOTS_GIT --work-tree=$HOME ls-tree -r master --name-only)
 
-chosen="$(echo -e "$dotfile_paths" | rofi -dmenu)"
+chosen="$(echo -e "$dotfile_paths" | rofi -dmenu -no-custom)"
 [ -f "$chosen" ] && $TERMINAL $EDITOR $chosen || dunstify "No such file"
---
+++ .config/rofi/screen-read.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# DEPENDENCIES: tesseract-ocr imagemagick scrot
+
+APP_NAME="Screen Reader"
+
+export LC_ALL=en_US.UTF-8
+
+LANG="$(echo -e "eng\nita\ndeu\nben" | rofi -dmenu -no-custom)"
+if [ -z "$LANG" ]; then 
+	dunstify -a "$APP_NAME" "Invalid Language" 
+	exit 1
+fi
+
+echo "Language set to $LANG"
+
+SCR_IMG=$(mktemp) # create tempfile
+trap "rm $SCR_IMG*" EXIT # make sure tempfiles get deleted afterwards
+
+#take screenshot of area
+scrot -s "$SCR_IMG".png -q 100
+# postprocess to prepare for OCR
+mogrify -modulate 100,0 -resize 400% "$SCR_IMG".png 
+# OCR in given language
+tesseract -l "$LANG" "$SCR_IMG".png "$SCR_IMG"
+xsel -bi < "$SCR_IMG".txt # pass to clipboard
+dunstify -a "$APP_NAME" "Text Copied to clipboard"
