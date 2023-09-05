dec4c20c9529002a625ffe2224af39111928403d
Rahat Zaman <rahatzamancse@gmail.com>
2023-09-05 11:03:46 -0600

    Removed hardtime nvim from vscode

--- .config/nvim/lua/plugins/hardtime.lua
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -1,5 +1,8 @@
 return {
    "m4xshen/hardtime.nvim",
    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+   cond = function()
+       return not vim.g.vscode
+   end,
    opts = {}
 }
