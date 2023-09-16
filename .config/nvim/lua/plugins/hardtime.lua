ca97b433fc0e42fcf58de5196bd5be1606eb9880
Shadmaan Hye <praptishadmaan@gmail.com>
2023-09-15 22:55:08 -0600

    removed nvim hardtime

--- .config/nvim/lua/plugins/hardtime.lua
+++
@@ -1,8 +0,0 @@
-return {
-   "m4xshen/hardtime.nvim",
-   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-   cond = function()
-       return not vim.g.vscode
-   end,
-   opts = {}
-}
